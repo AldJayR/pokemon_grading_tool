596b2ae147ea69016e6f42ab8947834793032590
AldJayR <aldwinjzs3@gmail.com>
2024-12-23 13:06:43 +0800

    added dependencies

--- build.sh
+++ build.sh
@@ -2,6 +2,16 @@
 # exit on error
 set -o errexit
 
+apt-get update
+apt-get install -y \
+    libgstgl-1.0.so.0 \
+    libgstcodecparsers-1.0.so.0 \
+    libavif.so.15 \
+    libenchant-2.so.2 \
+    libsecret-1.so.0 \
+    libmanette-0.2.so.0 \
+    libGLESv2.so.2
+
 pip install -r requirements.txt
 playwright install
 python manage.py collectstatic --no-input
