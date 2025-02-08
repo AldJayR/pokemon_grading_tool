70ebf11106b9a93ecc7653130425a20a15038344
AldJayR <aldwinjzs3@gmail.com>
2025-02-08 23:55:45 +0800

    fixed postgres

--- pokemon_grading_tool/settings.py
+++ pokemon_grading_tool/settings.py
@@ -181,7 +181,14 @@ LOGGING = {
 
 # Database Configuration
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["PGDATABASE"],
+        'USER': os.environ["PGUSER"],
+        'PASSWORD': os.environ["PGPASSWORD"],
+        'HOST': os.environ["PGHOST"],  
+        'PORT': os.environ["PGPORT"],    
+    }
 }
 
 CACHE_MIDDLEWARE_SECONDS = 3600
